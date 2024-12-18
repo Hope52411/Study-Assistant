c784436e8eeda89aaf7c9e5039ab6b4c51851a86
Hope52411 <13038044100@163.com>
2024-12-18 22:10:32 +0000

    2nd commit

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load if defined?(Dotenv)
+Dotenv::Rails.load if defined?(Dotenv)
 module StudyAssistant
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# 引入 DatabaseCleaner 清理数据库
+require "database_cleaner/active_record"
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
@@ -10,6 +13,22 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    # DatabaseCleaner 配置
+    setup do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.start
+    end
+
+    teardown do
+      DatabaseCleaner.clean
+    end
+
+    # 避免多次加载数据库清理器
+    at_exit { DatabaseCleaner.clean_with(:truncation) }
+
+    # 添加更多的测试辅助方法
+    def unique_email
+      "user_#{SecureRandom.hex(4)}@example.com"
+    end
   end
 end
