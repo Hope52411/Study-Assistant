98299a098552c3c546b0281cb2c5ed283b86be17
Hope52411 <13038044100@163.com>
2024-12-18 22:33:03 +0000

    5th commit

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(email: "test@example.com", password: "password")
+    sign_in @user
+  end
+
   test "should get index" do
     get home_index_url
     assert_response :success
