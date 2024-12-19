4b7168e071105e530534091a81d8d093cc9a1eca
Hope52411 <13038044100@163.com>
2024-12-19 18:47:12 +0000

    27th commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,13 @@
+// Rails UJS
+import Rails from "@rails/ujs"
+Rails.start()
+
+// Turbo
 import "@hotwired/turbo-rails"
+
+// Stimulus Controllers
 import "controllers"
+
+// Bootstrap and Popper
 import "bootstrap"
 import "@popperjs/core"
-import * as Rails from "@rails/ujs";
-Rails.start();
-
