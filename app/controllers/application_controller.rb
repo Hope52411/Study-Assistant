c45c5310ec6327dc3c8c1cc08728bf67c874ddd4
Hope52411 <13038044100@163.com>
2024-12-19 23:47:51 +0000

    32nd commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
+
+ before_action :authenticate_user!
+
  allow_browser versions: :modern
 
 
   def after_sign_in_path_for(resource)
     plans_path
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
   end
 end
