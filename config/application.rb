a6a8b07ce03bbd3f3fc4dbe4d781917bcc13c1c1
Hope52411 <13038044100@163.com>
2024-12-20 04:19:36 +0000

    41st commit

--- config/application.rb
+++ config/application.rb
@@ -5,16 +5,15 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Rails.load if defined?(Dotenv)
+
+# Ensure dotenv is loaded for specific environments
+require 'dotenv'
+Dotenv.load if %w[development test production].include?(Rails.env)
+
 module StudyAssistant
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
